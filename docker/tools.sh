d0771893e77fadc51747cb4c07ac24040203b874
Agnieszka Tatara <agnieszka.tatara@uxpin.com>
2018-11-21 11:39:43 +0100

    proper path

--- docker/tools.sh
+++ docker/tools.sh
@@ -2,7 +2,10 @@
 # shellcheck source=./helpers.sh
 source .uxpinci/helpers.sh
 
+cd /app/packages/uxpin-merge-cli
+capture_status make test-resources
+capture_status make build
 capture_status make check
 capture_status make test-ci
-cd ~/project/
+cd /app
 capture_status ./packages/uxpin-merge-cli/node_modules/.bin/codecov -F unittests -F uxpin_merge_cli
