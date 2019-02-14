cba7abb3da5b0fe597fc14a5e5af4c1179568d2e
Earl Debicki <bartosz.debicki@uxpin.com>
2019-02-14 15:57:09 +0100

    implememnt spred and external file prop types

--- packages/uxpin-merge-cli/src/steps/serialization/component/implementation/javascript/getDefaultComponentFrom.ts
+++ packages/uxpin-merge-cli/src/steps/serialization/component/implementation/javascript/getDefaultComponentFrom.ts
@@ -1,7 +1,16 @@
 import { readFile } from 'fs-extra';
-import { parse } from 'react-docgen';
+import { defaultHandlers, parse } from 'react-docgen';
+import externalProptypesHandler from 'react-docgen-external-proptypes-handler';
+import importedProptypesHandler from 'react-docgen-imported-proptype-handler';
 import { ComponentDoc } from 'react-docgen-typescript/lib';
 
-export function getDefaultComponentFrom(filePath:string):Promise<ComponentDoc> {
-  return readFile(filePath, { encoding: 'utf8' }).then(parse);
+export async function getDefaultComponentFrom(filePath:string):Promise<ComponentDoc> {
+    const fileContents:string = await readFile(filePath, { encoding: 'utf8' });
+
+    const handlers:any[] = defaultHandlers.concat([
+        externalProptypesHandler(filePath),
+        importedProptypesHandler(filePath),
+    ]);
+
+    return parse(fileContents, null, handlers);
 }
